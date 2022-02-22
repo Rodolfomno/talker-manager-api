83e32b5f1f4db26df7abe7b840830508754107b9
Rodolfomno <Rodolfo_mno@hotmail.com>
2022-02-22 20:40:07 -0300

    adiciona resolucao requisito 2

--- middlewares/getTalkers.js
+++ middlewares/getTalkers.js
@@ -13,4 +13,17 @@ getTalkers.get('/', async (_req, res) => {
     }
 });
 
+getTalkers.get('/:id', async (req, res) => {
+    try {
+    const { id } = req.params;
+    const talkerFile = await fs.readFile('./talker.json', 'utf-8');
+    const parsedTalker = JSON.parse(talkerFile);
+    const talker = parsedTalker.find((person) => person.id === Number(id));
+    if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(200).json(talker);
+    } catch (e) {
+        return console.log(e);
+    }
+});
+
 module.exports = getTalkers;
